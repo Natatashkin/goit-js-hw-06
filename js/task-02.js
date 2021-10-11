476fabd5754d5c65cef1fe6e5b83b2f3427d949a
Natatashkin <cs070283sng@gmail.com>
2021-10-12 00:21:59 +0300

    add first 3 tasks

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+console.log(list);
+
+const ingredientsList = ingredients.map((item) => {
+  const ingredient = document.createElement("li");
+  ingredient.textContent = item;
+  ingredient.classList.add("item");
+  return ingredient;
+});
+
+list.append(...ingredientsList);
