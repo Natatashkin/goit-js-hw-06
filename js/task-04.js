4d8863742b82373466ee48c772255332e9281970
Natatashkin <cs070283sng@gmail.com>
2021-10-17 16:52:30 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,21 +1,36 @@
 const increment = document.querySelector('button[data-action="increment"]');
 const decrement = document.querySelector('button[data-action="decrement"]');
-const counterValue = document.querySelector("#value");
+const counterValue = document.querySelector('#value');
 
-// var 3 с переменной как просил ментор, но второй делает тоже самое с функцией вместо переменной. я проверяла
+// var 4 c этим решением я не согласна, так мы не получаем значение спана, прописанное в html, и если руками в дкевтулзах заменить 0 на 25, счётчик продолжает считать от 0.
 
-increment.addEventListener("click", handleIncrement);
-decrement.addEventListener("click", handleDecrement);
+increment.addEventListener('click', handleIncrement);
+decrement.addEventListener('click', handleDecrement);
+
+let number = 0;
 
 function handleIncrement() {
-  const value = Number(counterValue.textContent);
-  counterValue.textContent = value + 1;
+  counterValue.textContent = ++number;
 }
 
 function handleDecrement() {
-  const value = Number(counterValue.textContent);
-  counterValue.textContent = value - 1;
+  counterValue.textContent = --number;
 }
+
+// var 3 с переменной как просил ментор, но второй делает тоже самое с функцией вместо переменной. я проверяла
+
+// increment.addEventListener('click', handleIncrement);
+// decrement.addEventListener('click', handleDecrement);
+
+// function handleIncrement() {
+//   const value = Number(counterValue.textContent);
+//   counterValue.textContent = value + 1;
+// }
+
+// function handleDecrement() {
+//   const value = Number(counterValue.textContent);
+//   counterValue.textContent = value - 1;
+// }
 
 // ===============================================================
 
@@ -29,8 +44,8 @@ function handleDecrement() {
 // ================================================================
 
 // var 2 - ну или двумя ))))
-// increment.addEventListener("click", handleIncrement);
-// decrement.addEventListener("click", handleDecrement);
+// increment.addEventListener('click', handleIncrement);
+// decrement.addEventListener('click', handleDecrement);
 
 // function handleIncrement() {
 //   const value = convertToNumber();
