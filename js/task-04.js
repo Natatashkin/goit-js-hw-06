3e0c7595e81c2593b51716632f73c5a8e28baa44
Natatashkin <cs070283sng@gmail.com>
2021-10-18 01:46:38 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,79 +1,42 @@
-const increment = document.querySelector('button[data-action="increment"]');
-const decrement = document.querySelector('button[data-action="decrement"]');
-const number = document.querySelector('#value');
-
-// var 4 c этим решением я не согласна, так мы не получаем значение спана, прописанное в html, и если руками в дкевтулзах заменить 0 на 25, счётчик продолжает считать от 0.
-
-increment.addEventListener('click', handleIncrement);
-decrement.addEventListener('click', handleDecrement);
-
-let counterValue = 0;
-
-function handleIncrement() {
-  number.textContent = ++counterValue;
-}
-
-function handleDecrement() {
-  number.textContent = --counterValue;
-}
-
-// var 3 с переменной как просил ментор, но второй делает тоже самое с функцией вместо переменной. я проверяла
+// const increment = document.querySelector('button[data-action="increment"]');
+// const decrement = document.querySelector('button[data-action="decrement"]');
+// const number = document.querySelector('#value');
 
 // increment.addEventListener('click', handleIncrement);
 // decrement.addEventListener('click', handleDecrement);
 
+// let counterValue = 0;
+
 // function handleIncrement() {
-//   const value = Number(counterValue.textContent);
-//   counterValue.textContent = value + 1;
+//   number.textContent = ++counterValue;
 // }
 
 // function handleDecrement() {
-//   const value = Number(counterValue.textContent);
-//   counterValue.textContent = value - 1;
+//   number.textContent = --counterValue;
 // }
 
-// ===============================================================
+const counter = {
+  counterValue: 0,
+  increment() {
+    this.counterValue += 1;
+  },
+  decrement() {
+    this.counterValue -= 1;
+  },
+};
 
-// Вспомогательная функция для перевода строки в число в вариантах решения 2 и 3
+const btnDecrement = document.querySelector('[data-action="decrement"]');
+const btnIncrement = document.querySelector('[data-action="increment"]');
+const valueEl = document.querySelector('#value');
 
-// function convertToNumber() {
-//   const value = Number(counterValue.textContent);
-//   return value;
-// }
+btnDecrement.addEventListener('click', function () {
+  //   console.log(-1);
+  counter.decrement();
+  valueEl.textContent = counter.counterValue;
+});
 
-// ================================================================
-
-// var 2 - ну или двумя ))))
-// increment.addEventListener('click', handleIncrement);
-// decrement.addEventListener('click', handleDecrement);
-
-// function handleIncrement() {
-//   const value = convertToNumber();
-//   counterValue.textContent = value + 1;
-// }
-
-// function handleDecrement() {
-//   const value = convertToNumber();
-//   counterValue.textContent = value - 1;
-// }
-
-// ================================================================
-//  var1 - все можно сделать одним обработчиком
-// increment.addEventListener("click", handleClick);
-// decrement.addEventListener("click", handleClick);
-
-// function handleClick(event) {
-//   const target = event.currentTarget.dataset.action;
-//   const value = convertToNumber();
-//   if (target === "increment") {
-//     counterValue.textContent = value + 1;
-//     return;
-//   }
-//   if (target === "decrement") {
-//     if (value === 0) {
-//       return;
-//     }
-//     counterValue.textContent = value - 1;
-//     return;
-//   }
-// }
+btnIncrement.addEventListener('click', function () {
+  //   console.log(+1);
+  counter.increment();
+  valueEl.textContent = counter.counterValue;
+});
